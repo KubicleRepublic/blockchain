c65999562ce40e7d65333af8a5e717981a6c3a1b
E <elyseemafranchuk@gmail.com>
2020-03-07 19:28:41 -0700

    can now upload files

--- source/web_server/web.py
+++ source/web_server/web.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, request
 from collections import defaultdict
+from werkzeug import secure_filename
 #from flask. import principal, permission, RoleNeed
 import json
 import os
@@ -21,21 +22,6 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 def hello():
     return "hello world, could u proceed to /home"
 
-@app.route("/home")
-def home():
-    return render_template('home.html')
-
-
-@app.route("/receive-count", methods=['GET'])
-def receive_count():
-
-    url = 'http://localhost:8080/get_votes'
-    returned = requests.get(url)
-    return returned.text
-
-
-
-
 @app.route("/vote-submission", methods=['GET','POST'])
 def vote_submission():
     if request.method == 'POST':
@@ -46,6 +32,8 @@ def vote_submission():
 
             EID = request.form["test1"]
             electee = request.form["options"]
+            #tokenfile = request.form["fileToUpload"]
+            #print(tokenfile)
 
             # if electee == 'A':
             #     newcount['Donald'] += 1
@@ -61,7 +49,49 @@ def vote_submission():
             # #response = "this is the answer from the node: " + x.text
             # #print(response)
             # print("----------")
-    return response.text
+            return response.text            
+            return render_template('success.html',name = f.filename)
+
+
+@app.route("/home")
+#use uploadScreen.html
+def home():
+    return render_template('uploadScreen.html')
+    vote_submission()
+
+@app.route('/success', methods =['POST'])
+def success():
+    if request.method == 'POST':
+        #If a vote is 'posted' then the file is saved in the dir
+        f = request.files['file']
+        f.save(f.filename)
+
+        #This is reading what candidate was voted for
+        url = 'http://localhost:8080/ballot'
+        electee = request.form["options"]
+        print("vote is " + electee)
+        Token = {'candidate' : electee}
+        response = requests.post(url, data = Token)
+        
+        return render_template("success.html", name = f.filename), response.text
+    
+    
+
+
+@app.route("/receive-count", methods=['GET'])
+def receive_count():
+
+    url = 'http://localhost:8080/get_votes'
+    returned = requests.get(url)
+    return returned.text
+
+#This is for reading the uploaded file
+
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0') #ssl_context='adhoc'
